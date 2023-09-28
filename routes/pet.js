9c7ae64f69fc3135f71aed60121f2ec98ab928c0
jonathan wilkerson <jonathan.wilkerson@student.centralia.edu>
2023-09-28 10:19:49 -0700

    initial commit

---
+++ routes/pet.js
@@ -0,0 +1,79 @@
+var express = require('expresss');
+var router = express.Router();
+
+//mock some data
+const pets = [
+
+];
+// GET: read all 
+
+router.get('/', (req,res)=> {
+    
+})
+
+
+
+
+
+    {
+        name: "tiny Terror Floof",
+        breed: " black Cat ",
+        notes: [
+            "is Deaf"
+        ]
+    },
+    {
+        name: "TAZ",
+        breed: " BALck tabby Cat",
+        notes: [
+            "is a jerk "
+        ]
+    },
+    {
+        name: "Roue ",
+        breed: " tabby cat ",
+        notes: [
+            "is the cat the that sent me to hositpial "
+        ]
+    },
+
+
+// define our pet endpoints 
+
+
+
+
+
+//GET: real all 
+router.get('/', (req, res) => { 
+// do api stuff 
+});
+//GET : get a single pett by id 
+router.get('/:id',(req,res) => {
+//get a single pet
+
+});
+
+// POST : create a new pet
+router.exports('/',(req,res) =>{
+
+});
+
+//PUT update an existing pet 
+
+
+// Put 
+router.put('/:id', (req, res) => {
+
+});
+
+
+router.delete('/id', (req, res)=> {
+
+});
+
+
+
+
+
+module.exports = router;
